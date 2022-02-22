5942b9a883b5d4341ecaafa3aeab620a00e83b4e
svadhis <imeian@gmail.com>
2022-02-22 16:49:18 +0100

    +

--- send_user_data.php
+++ send_user_data.php
@@ -2,18 +2,12 @@
 
 include('./database.php');
 
-if ($_POST['position'] == -1) {
-    $deleteUserStatement = $db->prepare("DELETE FROM users WHERE name = ?");
-    $deleteUserStatement->execute([$_POST['username']]);
+$checkUserStatement = $db->query("SELECT name FROM users WHERE name = '". $_POST['username'] ."'");
+if ($checkUserStatement->fetch()) {
+    $updateUserStatement = $db->prepare("UPDATE users SET position = ? WHERE name = ?");
+    $updateUserStatement->execute([$_POST['position'], $_POST['username']]);
 }
 else {
-    $checkUserStatement = $db->query("SELECT name FROM users WHERE name = '". $_POST['username'] ."'");
-    if ($checkUserStatement->fetch()) {
-        $updateUserStatement = $db->prepare("UPDATE users SET position = ? WHERE name = ?");
-        $updateUserStatement->execute([$_POST['position'], $_POST['username']]);
-    }
-    else {
-        $insertUserStatement = $db->prepare("INSERT INTO users (name, color, position) values (?, ?, ?)");
-        $insertUserStatement->execute([$_POST['username'], $_POST['color'], $_POST['position']]);
-    }
+    $insertUserStatement = $db->prepare("INSERT INTO users (name, color, position) values (?, ?, ?)");
+    $insertUserStatement->execute([$_POST['username'], $_POST['color'], $_POST['position']]);
 }
